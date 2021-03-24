86d36233b1ac1344fc142004728683ba3050ecd3
mariiayarema <MYarema1991@gmail.com>
2021-03-23 21:38:39 -0400

    Add diego line

--- src/test/java/com/hrms/pages/Julia.java
+++ src/test/java/com/hrms/pages/Julia.java
@@ -5,5 +5,6 @@ public class Julia {
     public void name(){
         System.out.println("My name is Jon");
         System.out.println("my name is Mariia");
+        System.out.println("my name is Diego");
     }
 }
