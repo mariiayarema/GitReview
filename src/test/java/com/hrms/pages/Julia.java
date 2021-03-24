1baed7a5792f57f0c38d37ef4fdc1728433ce091
mariiayarema <MYarema1991@gmail.com>
2021-03-23 21:43:06 -0400

    ..

--- src/test/java/com/hrms/pages/Julia.java
+++ src/test/java/com/hrms/pages/Julia.java
@@ -5,5 +5,6 @@ public class Julia {
     public void name(){
         System.out.println("My name is Jon");
         System.out.println("my name is Mariia");
+        System.out.println("I will get a job");
     }
 }
